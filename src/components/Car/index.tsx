e27fea61396f23d86aa4e2634ee221a2f4396879
Alessandre Livramento <aleliv74@gmail.com>
2021-11-11 13:18:25 -0300

    Chapter III - Consumindo API - Exibindo icones dinamicamente

--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -4,6 +4,7 @@ import { RectButtonProps } from 'react-native-gesture-handler'
 import GasolineSvg from '../../assets/gasoline.svg'
 import { CarDTO } from '../../dtos/CarDTO'
 import { formatCurrency } from '../../utils/formatted'
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon'
 
 import * as S from './styles'
 
@@ -12,6 +13,8 @@ interface Props extends RectButtonProps {
 }
 
 export function Car({ data, ...rest }: Props) {
+  const MotorIcon = getAccessoryIcon(data.fuel_type)
+
   return (
     <S.Container {...rest}>
       <S.Details>
@@ -25,7 +28,7 @@ export function Car({ data, ...rest }: Props) {
           </S.Rent>
 
           <S.type>
-            <GasolineSvg />
+            <MotorIcon />
           </S.type>
         </S.About>
       </S.Details>
--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 
-import speedSvg from '../../assets/speed.svg'
-import accelerationSvg from '../../assets/acceleration.svg'
-import forceSvg from '../../assets/force.svg'
-import gasolineSvg from '../../assets/gasoline.svg'
-import exchangeSvg from '../../assets/exchange.svg'
-import peopleSvg from '../../assets/people.svg'
-
 import { Accessory } from '../../components/Accessory'
 import { BackButton } from '../../components/BackButton'
 import { ImageSlider } from '../../components/ImageSlider'
 import { Button } from '../../components/Button'
 import { CarDTO } from '../../dtos/CarDTO'
+
 import { formatCurrency } from '../../utils/formatted'
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon'
 
 import * as S from './styles'
 
@@ -59,10 +54,13 @@ export function CarDetails() {
 
         <S.Accessories>
           {car.accessories.map(accessory => (
-            <Accessory key={accessory.type} name={accessory.name} icon={speedSvg} />
+            <Accessory
+              key={accessory.type}
+              name={accessory.name}
+              icon={getAccessoryIcon(accessory.type)}
+            />
           ))}
         </S.Accessories>
-
         <S.About>{car.about}</S.About>
       </S.Content>
 
