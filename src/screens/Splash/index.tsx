046b6b3e8b34f8eeb9a89e8822dc4312ca7ac3bf
Alessandre Livramento <aleliv74@gmail.com>
2021-11-19 23:21:36 -0300

    Chapter III - Animações - Primeira Animação

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Button, StyleSheet } from 'react-native'
+
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated'
 import * as S from './styles'
 
 export function Splash() {
+  const animation = useSharedValue(0)
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: animation.value }]
+    }
+  })
+
+  function handleAnimatationPosition() {
+    animation.value += 10
+  }
   return (
     <S.Container>
-      <Text style={{ color: 'white' }}>Splash Screen</Text>
+      <Animated.View style={[styles.box, animatedStyle]} />
+
+      <Button title="Mover" onPress={handleAnimatationPosition} />
     </S.Container>
   )
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  }
+})
