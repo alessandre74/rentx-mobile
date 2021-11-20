8e31c3418b9cdaa38d04afc054bd5b42a8218d5e
Alessandre Livramento <aleliv74@gmail.com>
2021-11-20 01:31:12 -0300

    Chapter III - Animações - Transições

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
-import { Button, StyleSheet } from 'react-native'
+import { Button, StyleSheet, Dimensions } from 'react-native'
 
-import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated'
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing
+} from 'react-native-reanimated'
 import * as S from './styles'
+
+const WIDTH = Dimensions.get('window').width
 
 export function Splash() {
   const animation = useSharedValue(0)
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: animation.value }]
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 500,
+            easing: Easing.bezier(0.73, 0.18, 0, 1.01)
+          })
+        }
+      ]
     }
   })
 
   function handleAnimatationPosition() {
-    animation.value += 10
+    animation.value = Math.random() * (WIDTH - 100)
   }
   return (
     <S.Container>
